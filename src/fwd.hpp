bff6e1c4021880df00e84adeec66b954b6ffc7de
polyndrom <polyndromdev@gmail.com>
2021-07-01 14:03:58 +1000

    add forward declaration

---
+++ src/fwd.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+template <class List>
+class node_type;
+
+template <class List>
+class node_pointer;
+
+template <typename List>
+class list_iterator;
+
+template <class T>
+class acid_list;
